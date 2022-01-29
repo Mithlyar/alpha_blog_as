abf800421392a6fc129eb6bb894b75224ae16719
Arek Sowa <Areksowa26@gmail.com>
2022-01-29 03:13:28 +0100

    Clean up layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ def index
 end
 
 def new
-@article = Article.new
+   @article = Article.new
 end
 
 def edit
@@ -17,22 +17,22 @@ end
 
 def create
 
-@article  = Article.new(article_params)
-if @article.save
-  flash[:notice] = "Article was created succesfully!"
- redirect_to (@article)
-else
- render 'new'
-end
+    @article  = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created succesfully!"
+     redirect_to (@article)
+    else
+     render 'new'
+    end
 end
 
 def update
-if @article.update(article_params)
-  flash[:notice] = "Article was updated succesfully!"
-  redirect_to (@article)
-else
-  render 'edit'
-end
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated succesfully!"
+      redirect_to (@article)
+    else
+      render 'edit'
+    end
 end
 
 def destroy
@@ -42,11 +42,11 @@ end
 
 private
 def set_article
-@article = Article.find(params[:id])
+   @article = Article.find(params[:id])
 end
 
- def article_params
-   params.require(:article).permit(:title, :desription)
- end
+def article_params
+  params.require(:article).permit(:title, :desription)
+end
 
 end
